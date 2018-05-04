091e1ff72a5c4329a1ed1b69960840a8c1504f7c
yongheng <1074827582@qq.com>
2018-05-04 10:13:25 +0800

    color

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,14 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+  background: #e1e1e1;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,40 +6,66 @@
     <div class="contents">
       <div class="items" v-for="item in colorList">
         <div class="cls" v-for="cls in item">
-        <span>品牌色</span>
-          <div class="background-panel" :style="{background: cls.HEX}"></div>
-          <div>{{cls.name}}</div>
-          <div class="HEX">HEX: {{cls.HEX}}</div>
-          <div class="RGB">RGB: {{cls.RGB}}</div>
+          <span class="cls-item">品牌色</span>
+          <div class="wrapper">
+            <div class="background-panel" :style="{background: cls.HEX}"></div>
+            <div class="color-value">
+              <div>{{cls.name}}</div>
+              <div class="HEX">HEX: {{cls.HEX}}</div>
+              <div class="RGB">RGB: {{cls.RGB}}</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <script>
-export default {
-  name: "ColorPalette",
-  data() {
-    return {
-      colorList: [
-        [{ name: "test1", HEX: "#ff0000", RGB: "rbg(255,0,0)" }],
-        [
-          { name: "test2", HEX: "#00ff00", RGB: "rbg(0,255,0)" },
-          { name: "test3", HEX: "#0000ff", RGB: "rbg(0,0,255)" }
-        ],
-        [{ name: "test3", HEX: "#0000ff", RGB: "rbg(0,0,255)" }]
-      ]
-    };
-  },
-  mounted() {
-    console.log(this.colorList);
-  }
-};
+  export default {
+    name: "ColorPalette",
+    data() {
+      return {
+        colorList: [
+          [{
+            name: "test1",
+            HEX: "#ff0000",
+            RGB: "rbg(255,0,0)"
+          }],
+          [{
+              name: "test2",
+              HEX: "#00ff00",
+              RGB: "rbg(0,255,0)"
+            },
+            {
+              name: "test3",
+              HEX: "#0000ff",
+              RGB: "rbg(0,0,255)"
+            }
+          ],
+          [{
+            name: "test3",
+            HEX: "#0000ff",
+            RGB: "rbg(0,0,255)"
+          }]
+        ]
+      };
+    },
+    mounted() {
+      console.log(this.colorList);
+    }
+  };
 </script>
+
 <style lang="stylus">
+.container {
+  background: #e1e1e1;
+}
+
 .title {
   width: 120px;
   padding: 10px 0;
+  margin: 20px 10px;
   border-bottom: 1px solid #ccc;
 }
 
@@ -50,19 +76,29 @@ export default {
     justify-content: flex-start;
     align-items: center;
 
-    .cls {
+    .wrapper {
+// -webkit-box-shadow: 0px 6px 18px 6px rgba(100,100,100,0.5); 
+box-shadow: 0px 6px 18px 2px rgba(25,25,25,0.15);
+      // box-shadow: 2px 2px 4px 1px rgba(0, 0, 0, 0.1), -2px 2px 4px 1px rgba(0, 0, 0, 0.1), 4px 4px 6px 2px rgba(50, 50, 50, 0.1), -4px 0 6px 2px rgba(50, 50, 50, 0.1), 8px 8px 20px 4px rgba(100, 100, 100, 0.1);
+      border-radius: 4px;
       margin-right: 40px;
+      font-size: 14px;
+      .color-value {
+        padding: 4px;
+      }
+    }
+    .cls-item {
+      font-size: 16px;
+      font-weight: 600;
+      color: #000;
     }
   }
 
   .background-panel {
     position: relative;
-    width: 100px;
-    height: 130px;
-    border-radius: 4px;
+    height: 110px;
+    border-radius: 4px 4px 0 0/4px 4px 0 0;
     margin: 8px 0;
-
-    // box-shadow: 2px 2px 1px 0 rgba(0, 0, 0, 0.1), 4px 4px 2px 0 rgba(50, 50, 50, 0.1), 8px 8px 4px 0 rgba(100, 100, 100, 0.1);
     &::after {
       content: '';
       display: block;
